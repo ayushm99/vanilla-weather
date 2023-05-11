00f778cb002b6aa212628fbcbbfcb58df0fab89e
ayushm99 <ayushmohanty99@gmail.com>
2023-05-11 19:44:57 -0400

    small commit

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ const searchInput = document.querySelector('#search-input');
 const searchBtn = document.querySelector('#search-button');
 
 // by default using Farenheit and similar units of measurement (mi, mph)
-var tempUnits = 'I';
+let tempUnits = 'I';
 
 
 searchBtn.addEventListener('click', function() {
     // Get the value from the search input field
-    var userInput = searchInput.value;
+    let userInput = searchInput.value;
 
     console.log(userInput);
     
     // Use the userInput to perform further actions, such as making an API request
-    var apiUrl = 'https://api.weatherbit.io/v2.0/current?';
-    var apiKey = 'f943111cb5f543a88f015d144317e1d0';
+    let apiUrl = 'https://api.weatherbit.io/v2.0/current?';
+    let apiKey = 'f943111cb5f543a88f015d144317e1d0';
 
-    var requestUrl = (typeof userInput === 'number') ? `${apiUrl}zipcode=${userInput}&key=${apiKey}&units=${tempUnits}` : 
+    let requestUrl = (typeof userInput === 'number') ? `${apiUrl}zipcode=${userInput}&key=${apiKey}&units=${tempUnits}` : 
         `${apiUrl}city=${userInput}&key=${apiKey}&units=${tempUnits}`;
 
     fetch(requestUrl)
